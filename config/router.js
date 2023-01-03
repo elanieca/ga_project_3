608797d472291d6c3ec1edd28701d1c924996e7a
elanieca <elanieca@hotmail.com>
2023-01-03 18:06:53 +0100

    updated routes

--- config/router.js
+++ config/router.js
@@ -2,4 +2,24 @@ import express from 'express';
 
 const Router = express.Router();
 
+Router.route('/books').get().post();
+
+Router.route('/books/search').get();
+
+Router.route('/books/:bookId').get().delete();
+
+Router.route('/genres').get().post();
+
+Router.route('/genres/:genreId/books').get().delete();
+
+Router.route('/register').post();
+
+Router.route('/login').post();
+
+Router.route('/users/:userId/books').get();
+
+Router.route('/users/:userId/favorites').get().post();
+
+Router.route('/users/:userId/favorites/:favBookId').delete();
+
 export default Router;
