8ec06807f51c8e460fb082d62307f4804328e2d4
Imogen Farr <pidgefarr@Apples-MacBook-Pro.local>
2023-01-04 10:00:32 +0000

    user model

---
+++ models/user.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import uniqueValidator from 'mongoose-unique-validator';
+import mongooseHidden from 'mongoose-hidden';
+import { emailRegex } from '../lib/stringTesters.js';
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  username: { type: String, required: true },
+  isAdmin: { type: Boolean },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: (email) => emailRegex.test(email)
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: (pw) =>
+      /(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(pw)
+  }
+});
+
+userSchema.pre('save', function encryptPassword(next) {
+  if (this.isModified('password')) {
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync());
+  }
+  next();
+});
+
+userSchema.methods.validatePassword = function validatePassword(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+userSchema.plugin(
+  mongooseHidden({ defaultHidden: { password: true, email: true } })
+);
+
+userSchema.plugin(uniqueValidator);
+
+export default mongoose.model('User', userSchema);
